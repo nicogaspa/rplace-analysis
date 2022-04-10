2a6a6312a74d5ee1759feccb797012b69630cd64
nicogaspa <nicologasparini94@gmail.com>
2022-04-10 16:43:07 +0200

    download script and gitignore

---
+++ get_data.py
@@ -0,0 +1,30 @@
+import os
+import urllib.request
+import zipfile
+
+OUTPUT_FOLDER = "data"
+
+
+def download_files(start_from=0, until=78):
+    for i in range(start_from, until + 1):
+        filename = f"2022_place_canvas_history-{str(i).zfill(12)}.csv.gzip"
+        print(f"Getting file: {filename}")
+        download_url = f"https://placedata.reddit.com/data/canvas-history/{filename}"
+        filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), OUTPUT_FOLDER, filename)
+        urllib.request.urlretrieve(download_url, filepath)
+        print(f"\tGot it!")
+
+
+def unzip_files(start_from=0, until=78):
+    for i in range(start_from, until + 1):
+        filename = f"2022_place_canvas_history-{str(i).zfill(12)}.csv.gzip"
+        print(f"Unzipping file: {filename}")
+        output_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), OUTPUT_FOLDER)
+        with zipfile.ZipFile(filename, 'r') as zip_ref:
+            zip_ref.extractall(output_folder)
+        print(f"\tDone!")
+
+
+if __name__ == "__main__":
+    download_files()
+    unzip_files()
